54c2f9cdf275e8c7e999ecab2d6569c83371470c
pangyo255 <pangyo255@gmail.com>
2018-08-27 17:47:41 +0900

    DragReorder 중도 커밋

--- app/src/main/java/com/zxcv5500/toyproject/book/apc/c18_process/DragReorder.java
+++ app/src/main/java/com/zxcv5500/toyproject/book/apc/c18_process/DragReorder.java
@@ -2,16 +2,27 @@ package com.zxcv5500.toyproject.book.apc.c18_process;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zxcv5500.toyproject.R;
+
+import java.util.zip.Inflater;
 
 public class DragReorder extends Activity {
 	// 로그용 태그
@@ -19,7 +30,7 @@ public class DragReorder extends Activity {
 	ReorderScrollView mScroll;
 	LinearLayout mList;
 	WindowManager mWindowManager;
-	WindowManager.LayoutParams mParamBar;
+	WindowManager.LayoutParams mParamsBar;
 	WindowManager.LayoutParams mParamImage;
 	int mStartDragIdx;
 	int mStartDragY;
@@ -42,6 +53,96 @@ public class DragReorder extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dragreorder);
+
+		String[] arTerm = new String[] {
+				"00.입춘", "01.우수", "02.경칩", "03.춘분","04.청명", "05.곡우",
+				"06.입하", "07.소만", "08.망종", "09.하지","10.소서", "11.대서",
+				"12.입추", "13.처서", "14.백로", "15.추분","16.한로", "17.상강",
+				"18.입동", "19.소설", "20.대설", "21.동지","22.소한", "23.대한"};
+		mScroll = (ReorderScrollView) findViewById(R.id.scroll);
+		mScroll.mParent = this;
+		mList = findViewById(R.id.list);
+
+		// 테스트 항목 삽입
+		for (int i = 0; i < arTerm.length; i++) {
+			LayoutInflater inflater = (LayoutInflater) getSystemService(
+					Context.LAYOUT_INFLATER_SERVICE
+			);
+			LinearLayout item = (LinearLayout) inflater.inflate(R.layout.listitem, null);
+			TextView title = (TextView) item.findViewById(R.id.title);
+			title.setText(arTerm[i]);
+			mList.addView(item);
+
+			// 항목 클릭하면 텍스트만 읽어서 출력
+			item.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					TextView title = v.findViewById(R.id.title);
+					Toast.makeText(DragReorder.this, title.getText(), Toast.LENGTH_SHORT).show();
+				}
+			});
+
+			// 클릭은 일단 날라옴. 드래그를 시작했으면 클릭 동작은 하지 않되
+			// 이때 띡하는 소리를 어찌할 방법이 없다.
+			ImageButton btn = item.findViewById(R.id.btnreorder);
+			btn.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					if (mDragging == false) {
+						Log.d(TAG, "onClick: ");
+						mList.removeViewAt(mStartDragIdx);
+					}
+				}
+			});
+		}
+
+		prepareDragReorder();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mWindowManager.removeView(mDragBar);
+		mWindowManager.removeView(mDragImage);
+	}
+
+	/**
+	 * 드래그 처리에 필요한 이미지 및 배치 파라미터 등을 생성해 둠.
+	 */
+	void prepareDragReorder() {
+		mSlop = ViewConfiguration.get(this).getScaledTouchSlop();
+
+		// 배치 파라미터 미리 생성해 둠
+		mParamsBar = new WindowManager.LayoutParams();
+		mParamsBar.gravity = Gravity.TOP;
+		mParamsBar.x = 0;
+		mParamsBar.y = 0;
+		mParamsBar.height = DRAGBARTHICK;
+		mParamsBar.width = WindowManager.LayoutParams.MATCH_PARENT;
+		mParamsBar.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+				| WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+				| WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+				| WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+		mParamsBar.format = PixelFormat.TRANSLUCENT;
+		mParamsBar.windowAnimations = 0;
+
+		// 드래그 바 생성해서 배치해 놓고 살짝 숨겨 놓음
+		mDragBar = new ImageView(this);
+		int backGroundColor = Color.parseColor("red");
+		mDragBar.setBackgroundColor(backGroundColor);
+		mDragBar.setVisibility(View.GONE);
+		mWindowManager = (WindowManager) getSystemService("window");
+		mWindowManager.addView(mDragBar, mParamsBar);
+
+		// 드래그 이미지 생성해서 숨겨 둠
+		mParamImage = new WindowManager.LayoutParams();
+		mParamImage.copyFrom(mParamsBar);
+		mParamImage.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		mParamImage.alpha = 0.5f;
+
+		mDragImage = new ImageView(this);
+		mDragImage.setVisibility(View.GONE);
+		mWindowManager.addView(mDragImage, mParamImage);
 	}
 
 	// 스크롤 뷰가 터치를 가로채서 전달한다.
